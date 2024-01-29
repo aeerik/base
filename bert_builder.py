2001bce3949369360032bf871a9cf9f9e44e290e
Erik Aerts <erik.willem.aerts@gmail.com>
2024-01-29 14:11:40 +0100

    bert v1

---
+++ bert_builder.py
@@ -0,0 +1,47 @@
+import torch
+
+from torch import nn
+import torch.nn.functional as f
+
+class AttentionHead(nn.Module):
+
+    def __init__(self, dim_inp, dim_out, drop_prob):
+        super(AttentionHead, self).__init__()
+
+        self.dim_inp = dim_inp
+        self.drop_prob = drop_prob
+        self.dropout = nn.Dropout(drop_prob)
+        self.q = nn.Linear(dim_inp, dim_out)
+        self.k = nn.Linear(dim_inp, dim_out)
+        self.v = nn.Linear(dim_inp, dim_out)
+
+    def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor = None):
+        query, key, value = self.q(input_tensor), self.k(input_tensor), self.v(input_tensor)
+
+        scale = query.size(1) ** 0.5
+        scores = torch.bmm(query, key.transpose(1, 2)) / scale
+
+        scores = scores.masked_fill_(attention_mask, -1e9)
+        attn = f.softmax(scores, dim=-1)
+        attn = self.dropout(attn)
+        context = torch.bmm(attn, value)
+
+        return context
+
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, num_heads, dim_inp, dim_out):
+        super(MultiHeadAttention, self).__init__()
+
+        self.heads = nn.ModuleList([
+            AttentionHead(dim_inp, dim_out) for _ in range(num_heads)
+        ])
+        self.linear = nn.Linear(dim_out * num_heads, dim_inp)
+        self.norm = nn.LayerNorm(dim_inp)
+
+    def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor):
+        s = [head(input_tensor, attention_mask) for head in self.heads]
+        scores = torch.cat(s, dim=-1)
+        scores = self.linear(scores)
+        return self.norm(scores)
