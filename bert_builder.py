4314e00cb6732f3d2589d032be334ece2167f276
Erik Aerts <erik.willem.aerts@gmail.com>
2024-02-16 13:41:31 +0100

    1602

--- bert_builder.py
+++ bert_builder.py
@@ -4,18 +4,16 @@ from torch import nn
 import torch.nn.functional as f
 from embedding import JointEmbedding
 
-
 class AttentionHead(nn.Module):
 
-    def __init__(self, dim_inp, dim_out, drop_prob):
+    def __init__(self, dim_embedding, drop_prob):
         super(AttentionHead, self).__init__()
 
-        self.dim_inp = dim_inp
-        self.drop_prob = drop_prob
+        self.dim_embedding = dim_embedding
         self.dropout = nn.Dropout(drop_prob)
-        self.q = nn.Linear(dim_inp, dim_out)
-        self.k = nn.Linear(dim_inp, dim_out)
-        self.v = nn.Linear(dim_inp, dim_out)
+        self.q = nn.Linear(self.dim_embedding, self.dim_embedding)
+        self.k = nn.Linear(self.dim_embedding, self.dim_embedding)
+        self.v = nn.Linear(self.dim_embedding, self.dim_embedding)
 
     def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor = None):
         query, key, value = self.q(input_tensor), self.k(input_tensor), self.v(input_tensor)
@@ -33,62 +31,70 @@ class AttentionHead(nn.Module):
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, num_heads, dim_inp, dim_out, drop_prob):
+    def __init__(self, num_heads, dim_embedding, drop_prob):
         super(MultiHeadAttention, self).__init__()
 
         self.heads = nn.ModuleList([
-            AttentionHead(dim_inp, dim_out,drop_prob) for _ in range(num_heads)
+            AttentionHead(dim_embedding,drop_prob) for _ in range(num_heads)
         ])
-        self.linear = nn.Linear(dim_out * num_heads, dim_inp)
-        self.norm = nn.LayerNorm(dim_inp)
+        self.linear = nn.Linear(dim_embedding * num_heads, dim_embedding)
+        self.norm = nn.LayerNorm(dim_embedding)
 
     def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor):
         s = [head(input_tensor, attention_mask) for head in self.heads]
         scores = torch.cat(s, dim=-1)
         scores = self.linear(scores)
         return self.norm(scores)
-    
-class Encoder(nn.Module):
+class resEncoder(nn.Module):
 
-    def __init__(self, dim_inp, dim_out, attention_heads, dropout_prob):
-        super(Encoder, self).__init__()
+    def __init__(self, dim_embedding, attention_heads, dropout_prob):
+        super(resEncoder, self).__init__()
         self.dropout_prob = dropout_prob
         self.attention_heads = attention_heads
-        self.dim_inp = dim_inp
-        self.dim_out = dim_out
+        self.dim_embedding = dim_embedding
+        self.dense = nn.Linear(self.dim_embedding, self.dim_embedding)
+        self.layer_norm = nn.LayerNorm(self.dim_embedding)
+        self.dropout = nn.Dropout(self.dropout_prob)       
         
-        
-        self.attention = MultiHeadAttention(self.attention_heads, self.dim_inp, self.dim_out, self.dropout_prob)  # batch_size x sentence size x dim_inp
+        self.attention = MultiHeadAttention(self.attention_heads, self.dim_embedding, self.dropout_prob)  
         self.feed_forward = nn.Sequential(
-            nn.Linear(self.dim_inp, self.dim_out),
-            nn.Dropout(self.dropout_prob),
+            nn.Linear(self.dim_embedding, self.dim_embedding),
             nn.GELU(),
-            nn.Linear(self.dim_out, self.dim_inp),
+            nn.Linear(self.dim_embedding, self.dim_embedding),
             nn.Dropout(self.dropout_prob)
         )
-        self.norm = nn.LayerNorm(self.dim_inp)
+        self.norm = nn.LayerNorm(self.dim_embedding)
 
     def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor):
+        x = input_tensor
         context = self.attention(input_tensor, attention_mask)
-        res = self.feed_forward(context)
-        return self.norm(res)
-    
-class BERT(nn.Module):
 
-    def __init__(self, vocab_size, max_length, dim_inp, dim_out, attention_heads, num_encoders, dropout_prob):
-        super(BERT, self).__init__()
+        hidden_states = self.dense(context)
+        hidden_states = self.dropout(hidden_states)
+        x = x + hidden_states
+        x = self.layer_norm(x)
+        
+        res = x
+        x = self.feed_forward(x)
+        x = x + res
+        x = self.layer_norm(x)
+        return x
+
+class BERT_pt(nn.Module):
+
+    def __init__(self, vocab_size, max_length, dim_embedding, attention_heads, num_encoders, dropout_prob):
+        super(BERT_pt, self).__init__()
         self.attention_heads = attention_heads
         self.max_length = max_length
         self.vocab_size = vocab_size
-        self.dim_inp = dim_inp  
-        self.dim_out = dim_out
+        self.dim_embedding = dim_embedding
         self.num_encoders = num_encoders
         self.dropout_prob = dropout_prob  
 
-        self.embedding = JointEmbedding(self.dim_inp, self.vocab_size, self.max_length, self.dropout_prob)
-        self.encoders = nn.ModuleList([Encoder(self.dim_inp, self.dim_out, self.attention_heads, self.dropout_prob) for _ in range(self.num_encoders)])
+        self.embedding = JointEmbedding(self.dim_embedding, self.vocab_size, self.max_length, self.dropout_prob)
+        self.encoders = nn.ModuleList([resEncoder(self.dim_embedding, self.attention_heads, self.dropout_prob) for _ in range(self.num_encoders)])
 
-        self.token_prediction_layer = nn.Linear(self.dim_inp, self.vocab_size)
+        self.token_prediction_layer = nn.Linear(self.dim_embedding, self.vocab_size)
         self.softmax = nn.LogSoftmax(dim=-1)
 
     def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor):
@@ -98,4 +104,49 @@ class BERT(nn.Module):
 
         token_predictions = self.token_prediction_layer(embedded)
 
-        return self.softmax(token_predictions)
+        return self.softmax(token_predictions)    
+
+class BERT_ft(nn.Module):
+
+    def __init__(self, vocab_size, max_length, dim_embedding, dim_hidden, attention_heads, num_encoders, dropout_prob, num_ab):
+        super(BERT_ft, self).__init__()
+        self.attention_heads = attention_heads
+        self.max_length = max_length
+        self.vocab_size = vocab_size
+        self.dim_embedding = dim_embedding
+        self.dim_hidden = dim_hidden    
+        self.num_encoders = num_encoders
+        self.dropout_prob = dropout_prob  
+
+        self.embedding = JointEmbedding(self.dim_embedding, self.vocab_size, self.max_length, self.dropout_prob)
+        self.encoders = nn.ModuleList([resEncoder(self.dim_embedding, self.attention_heads, self.dropout_prob) for _ in range(self.num_encoders)])
+
+        
+        self.softmax = nn.LogSoftmax(dim=-1)
+        self.BC = [BC_Ab(self.dim_embedding, self.dim_hidden).to(device) for _ in range(num_ab)]
+
+    def forward(self, input_tensor: torch.Tensor, attention_mask: torch.Tensor):
+        embedded = self.embedding(input_tensor)
+        for layer in self.encoders:
+            embedded = layer(embedded, attention_mask)
+        
+        cls_tokens = embedded[:, 0, :]
+        resistance_predictions = torch.cat([network(cls_tokens) for network in self.BC], dim=1)
+        return resistance_predictions 
+
+class BC_Ab(nn.Module): 
+    def __init__(self, emb_dim: int, hidden_dim: int):
+        super(BC_Ab, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.emb_dim = emb_dim
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(self.emb_dim, self.hidden_dim),
+            nn.ReLU(),
+            nn.LayerNorm(self.hidden_dim),
+            nn.Linear(self.hidden_dim, 1), # binary classification (S:0 | R:1)
+        )
+           
+    def forward(self, X):
+        # X is the CLS token of the BERT model
+        return self.classifier(X)
